78293cf3ae7bb97869c50153b5f2416c444328ad
System User <jamesgarzon92@gmail.com>
2015-08-28 17:47:24 -0500

    Corrigiendo la interfaz

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular-route.min.js"></script>
+		
+
 		<script src="js/index.js"></script>
 		<script src="js/controllers/PedidosController.js"></script>
 
@@ -16,51 +18,74 @@
 		<title>Instruequipos</title>
 
 	<body >
+<nav class="navbar navbar-static-top bg-primary">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
 
-	<div class="" data-example-id="inverted-navbar">
-		<!-- <nav class="navbar navbar-default navbar-fixed-top"> //navbar-fixed-top es para que permaneza en la pantalla -->
-		<nav class="navbar navbar-default">
-			<div class="container-fluid" style="box-shadow: 0 0 15px black;">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-9" aria-expanded="false"style=" margin-top: 30px;">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a href="#/" class="navbar-brand h2">  <span class="ionicons ion-home" aria-hidden="true"></span> Instruequipos</a>
-		
-				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="navbar-collapse collapse" id="navbar-collapse-9" aria-expanded="false" style="height: 1px; text-align: center; ">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#/" class="h2" data-toggle="collapse" data-target="#navbar-collapse-9">Home</a></li>
-						<li><a href="#/pedidos" class="h2" data-toggle="collapse" data-target="#navbar-collapse-9">Órdenes de Pedido</a></li>
-						<li><a href="#/produccion" class="h2">Ordenes de producción</a></li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container-fluid -->
-		</nav>
-	</div>
-	<div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+
+
 
  <div ng-view></div>
 
+<script src="lib/jquery/jquery.min.js"></script>
+<script src="lib/jquery/jquery.min.map"></script>
 
-
-
-<link rel="stylesheet" type="text/css" href="css/style.css">
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="lib/jquery/jquery.min.js"></script>
 
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="lib/boostrap/css/bootstrap.min.css">
 
-	<!-- Optional theme -->
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"> -->
-	<link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="css/bootstrap/bootstrap.min.js"></script>
+	<script src="lib/boostrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
